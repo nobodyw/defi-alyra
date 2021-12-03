079a960b7f5dcc127b2fbc0bd80f6cd64297c97d
nobodyw <michael.wong@epitech.eu>
2021-12-03 23:20:23 +0100

    deploy contract SwapFactory, SwapPair

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,23 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
     }
-  }
+  },
 };
